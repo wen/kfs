2616aa23e3c5d36b5521eec5a0bf8970c2ca97e2
Ye WEN <yewen@student.42.fr>
2022-06-05 21:52:35 +0200

    add comments

--- boot.s
+++ boot.s
@@ -13,7 +13,7 @@
 .section .bss
 .align 16
 stack_bottom:
-.skip 16384 # 16 KiB
+.skip 16384 # stack size: 16 KiB
 stack_top:
 
 .section .text
@@ -21,7 +21,7 @@ stack_top:
 .type _start, @function
 _start:
 	mov $stack_top, %esp
-	call kernel_main
+	call kernel_main # call C main function
 	cli
 1:	hlt
 	jmp 1b
