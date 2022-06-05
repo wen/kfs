839c781be4ec3196a68965351a0f5890bee8326d
Ye WEN <yewen@student.42.fr>
2022-06-05 14:51:27 +0200

    add banner

--- kernel.c
+++ kernel.c
@@ -7,14 +7,14 @@ static const size_t VGA_HEIGHT = 25;
 size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
-uint16_t* terminal_buffer;
+uint16_t *terminal_buffer;
 
 void terminal_initialize(void)
 {
 	terminal_row = 0;
 	terminal_column = 0;
 	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-	terminal_buffer = (uint16_t*) 0xB8000;
+	terminal_buffer = (uint16_t*)0xB8000;
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
 			const size_t index = y * VGA_WIDTH + x;
@@ -23,24 +23,16 @@ void terminal_initialize(void)
 	}
 }
 
-void terminal_setcolor(uint8_t color)
-{
-	terminal_color = color;
-}
-
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
-{
-	const size_t index = y * VGA_WIDTH + x;
-	terminal_buffer[index] = vga_entry(c, color);
-}
-
 void ft_putchar(char c)
 {
+	size_t index;
+
 	if (c == '\n') {
 		terminal_column = 0;
 		++terminal_row;
 	} else {
-		terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+		index = terminal_row * VGA_WIDTH + terminal_column;
+		terminal_buffer[index] = vga_entry(c, terminal_color);
 	}
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
@@ -61,5 +53,19 @@ void kernel_main(void)
 {
 	terminal_initialize();
 
-	ft_putstr("Hello, kernel World!\n");
+	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_CYAN, VGA_COLOR_BLACK);
+	ft_putchar('\n');
+	ft_putstr("@@@  @@@  @@@@@@@@   @@@@@@           @@@    @@@@@@\n");
+	ft_putstr("@@@  @@@  @@@@@@@@  @@@@@@@          @@@@   @@@@@@@@\n");
+	ft_putstr("@@!  !@@  @@!       !@@             @@!@!        @@@\n");
+	ft_putstr("!@!  @!!  !@!       !@!            !@!!@!       @!@\n");
+	ft_putstr("@!@@!@!   @!!!:!    !!@@!!        @!! @!!      !!@\n");
+	ft_putstr("!!@!!!    !!!!!:     !!@!!!      !!!  !@!     !!:\n");
+	ft_putstr("!!: :!!   !!:            !:!     :!!:!:!!:   !:!\n");
+	ft_putstr(":!:  !:!  :!:           !:!      !:::!!:::  :!:\n");
+	ft_putstr(" ::  :::   ::       :::: ::           :::   :: :::::\n");
+	ft_putstr(" :   :::   :        :: : :            :::   :: : :::\n");
+	ft_putchar('\n');
+	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	ft_putstr("Hello, world!\n");
 }
