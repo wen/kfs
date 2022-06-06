7f2e523108f7bdc3d95dfb9760d1e84aaa406566
Ye WEN <yewen@student.42.fr>
2022-06-06 23:42:29 +0200

    add some macros

--- libk/string.h
+++ libk/string.h
@@ -1,6 +1,11 @@
 #ifndef STRING_H
 #define STRING_H
+
 #include <stddef.h>
+
+#define TODIGIT(c)	((c) - '0')
+#define ISDIGIT(c)	((unsigned)FT_TODIGIT(c) <= 9)
+#define TOCHAR(n)	((n) + '0')
 
 void *memset(void *b, int c, size_t len);
 void *memcpy(void *dst, const void *src, size_t n);
