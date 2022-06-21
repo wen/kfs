6975c8f576e0787ef966f8909fdc2bf9b4906f7d
Ye WEN <yewen@student.42.fr>
2022-06-21 17:59:36 +0200

    rename cursor_x cursor_y

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -11,15 +11,15 @@ void shell(void);
 static const uint8_t VGA_WIDTH = 80;
 static const uint8_t VGA_HEIGHT = 25;
 
-uint8_t krow;
-uint8_t kcolumn;
+uint8_t cursor_y;
+uint8_t cursor_x;
 uint8_t kcolor;
 uint16_t *kbuffer;
 
 void kinit(void)
 {
-	krow = 0;
-	kcolumn = 0;
+	cursor_y = 0;
+	cursor_x = 0;
 	kcolor = vga_entry_color(VGA_LIGHT_GREY, VGA_BLACK);
 	kbuffer = (uint16_t*)0xB8000;
 	for (uint16_t y = 0; y < VGA_HEIGHT; y++) {
@@ -32,7 +32,7 @@ void kinit(void)
 
 void set_cursor(void)
 {
-	uint16_t pos = krow * 80 + kcolumn;
+	uint16_t pos = cursor_y * 80 + cursor_x;
     outb(0x3D4, 14);
     outb(0x3D5, (pos >> 8));
     outb(0x3D4, 15);
@@ -41,30 +41,30 @@ void set_cursor(void)
 
 void scroll(void)
 {
-	if (krow >= 25) {
+	if (cursor_y >= 25) {
 		for (uint16_t i = 0; i < VGA_WIDTH * 24; ++i)
 			kbuffer[i] = kbuffer[i + VGA_WIDTH];
 		for (uint16_t i = 24*VGA_WIDTH; i < VGA_WIDTH * 25; ++i)
 			kbuffer[i] = vga_entry(' ', kcolor);
-		krow = 24;
+		cursor_y = 24;
 	}
 }
 
 void kputchar(char c)
 {
-	size_t index;
+	uint16_t index;
 
 	if (c == '\n') {
-		kcolumn = 0;
-		++krow;
-	} else {
-		index = krow * VGA_WIDTH + kcolumn;
+		cursor_x = 0;
+		++cursor_y;
+	} else if (c >= ' ') {
+		index = cursor_y * VGA_WIDTH + cursor_x;
 		kbuffer[index] = vga_entry(c, kcolor);
-		++kcolumn;
+		++cursor_x;
 	}
-	if (kcolumn == VGA_WIDTH) {
-		kcolumn = 0;
-		++krow;
+	if (cursor_x == VGA_WIDTH) {
+		cursor_x = 0;
+		++cursor_y;
 	}
 
 	scroll();
