76453ef2b131461f136b56aa8b1928223259f6e2
Ye WEN <yewen@student.42.fr>
2022-07-23 18:20:34 +0200

    add heap.c

---
+++ kernel/heap.c
@@ -0,0 +1,38 @@
+#include "heap.h"
+
+extern uint32_t end;
+uint32_t placement_addr = (uint32_t)&end;
+
+uint32_t kmalloc_int(uint32_t sz, int align, uint32_t *phys)
+{
+	if (align == 1 && (placement_addr & 0xfffff000)) {
+		placement_addr &= 0xfffff000;
+		placement_addr += 0x1000;
+	}
+	if (phys)
+		*phys = placement_addr;
+	uint32_t tmp = placement_addr;
+	placement_addr += sz;
+
+	return tmp;
+}
+
+uint32_t kmalloc_a(uint32_t sz)
+{
+	return kmalloc_int(sz, 1, 0);
+}
+
+uint32_t kmalloc_p(uint32_t sz, uint32_t *phys)
+{
+	return kmalloc_int(sz, 0, phys);
+}
+
+uint32_t kmalloc_ap(uint32_t sz, uint32_t *phys)
+{
+	return kmalloc_int(sz, 1, phys);
+}
+
+uint32_t kmalloc(uint32_t sz)
+{
+	return kmalloc_int(sz, 0, 0);
+}
--- kernel/heap.h
+++ kernel/heap.h
@@ -1,3 +1,12 @@
 #ifndef HEAP_H
 #define HEAP_H
+
+#include <stdint.h>
+
+uint32_t kmalloc_int(uint32_t sz, int align, uint32_t *phys);
+uint32_t kmalloc_a(uint32_t sz);
+uint32_t kmalloc_p(uint32_t sz, uint32_t *phys);
+uint32_t kmalloc_ap(uint32_t sz, uint32_t *phys);
+uint32_t kmalloc(uint32_t sz);
+
 #endif
