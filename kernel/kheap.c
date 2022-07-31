b8d291388890ddd49ed1e1f94996c35a6149e1df
Ye WEN <yewen@student.42.fr>
2022-07-31 18:08:23 +0200

    update kheap

--- kernel/kheap.c
+++ kernel/kheap.c
@@ -2,12 +2,12 @@
 #include "paging.h"
 #include "heap.h"
 
-extern uint32_t end;
-uint32_t placement_addr = (uint32_t)&end;
+extern uintptr_t end;
+uintptr_t placement_addr = (uintptr_t)&end;
 extern page_dir_t *kernel_dir;
 heap_t *kheap;
 
-uint32_t kmalloc_int(uint32_t sz, int align, uint32_t *phys)
+void *kmalloc_int(size_t sz, int align, uintptr_t *phys)
 {
 	if (kheap) {
 		void *addr = alloc(sz, (uint8_t)align, kheap);
@@ -15,7 +15,7 @@ uint32_t kmalloc_int(uint32_t sz, int align, uint32_t *phys)
 			page_t *page = get_page((uint32_t)addr, 0, kernel_dir);
 			*phys = page->frame * PAGE_SIZE + ((uint32_t)addr & 0xfff);
 		}
-		return (uint32_t)addr;
+		return addr;
 	}
 
 	if (align == 1 && !IS_ALIGNED(placement_addr))
@@ -23,7 +23,7 @@ uint32_t kmalloc_int(uint32_t sz, int align, uint32_t *phys)
 
 	if (phys)
 		*phys = placement_addr;
-	uint32_t tmp = placement_addr;
+	void *tmp = (void*)placement_addr;
 	placement_addr += sz;
 	return tmp;
 }
@@ -33,22 +33,22 @@ void kfree(void *p)
 	free(p, kheap);
 }
 
-uint32_t kmalloc_a(uint32_t sz)
+void *kmalloc_a(size_t sz)
 {
 	return kmalloc_int(sz, 1, 0);
 }
 
-uint32_t kmalloc_p(uint32_t sz, uint32_t *phys)
+void *kmalloc_p(size_t sz, uintptr_t *phys)
 {
 	return kmalloc_int(sz, 0, phys);
 }
 
-uint32_t kmalloc_ap(uint32_t sz, uint32_t *phys)
+void *kmalloc_ap(size_t sz, uintptr_t *phys)
 {
 	return kmalloc_int(sz, 1, phys);
 }
 
-uint32_t kmalloc(uint32_t sz)
+void *kmalloc(size_t sz)
 {
 	return kmalloc_int(sz, 0, 0);
 }
--- kernel/kheap.h
+++ kernel/kheap.h
@@ -1,16 +1,17 @@
 #ifndef KHEAP_H
 #define KHEAP_H
 
+#include <stddef.h>
 #include <stdint.h>
 
 #define KHEAP_START 0xc0000000
 #define KHEAP_INITIAL_SIZE 0x100000
 
-uint32_t kmalloc_int(uint32_t sz, int align, uint32_t *phys);
-uint32_t kmalloc_a(uint32_t sz);
-uint32_t kmalloc_p(uint32_t sz, uint32_t *phys);
-uint32_t kmalloc_ap(uint32_t sz, uint32_t *phys);
-uint32_t kmalloc(uint32_t sz);
+void *kmalloc_int(size_t sz, int align, uintptr_t *phys);
+void *kmalloc_a(size_t sz);
+void *kmalloc_p(size_t sz, uintptr_t *phys);
+void *kmalloc_ap(size_t sz, uintptr_t *phys);
+void *kmalloc(size_t sz);
 void kfree(void *p);
 
 #endif
