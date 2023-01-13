65634266beac76eab6555f23d3314375cdccef66
Ye WEN <yewen@student.42.fr>
2023-01-13 16:51:10 +0100

    add vec

---
+++ libc/vec.c
@@ -0,0 +1,70 @@
+#include "vec.h"
+#include "string.h"
+#include "kheap.h"
+
+vec_t create_vec(size_t max_size, cmp_t cmp)
+{
+	vec_t ret;
+
+	ret.array = kmalloc(max_size * sizeof(void*));
+	bzero(ret.array, max_size * sizeof(void*));
+	ret.size = 0;
+	ret.max_size = max_size;
+	ret.cmp = cmp;
+
+	return ret;
+}
+
+vec_t place_vec(void *addr, size_t max_size, cmp_t cmp)
+{
+	vec_t ret;
+
+	ret.array = addr;
+	bzero(ret.array, max_size * sizeof(void*));
+	ret.size = 0;
+	ret.max_size = max_size;
+	ret.cmp = cmp;
+
+	return ret;
+}
+
+void destroy_vec(vec_t *vec)
+{
+	(void)vec;
+	//kfree(vec->array);
+}
+
+void insert_vec(void *item, vec_t *vec)
+{
+	size_t i;
+	cmp_t cmp = vec->cmp;
+
+	for (i = 0; i < vec->size && cmp(vec->array[i], item); ++i)
+		{} // no operation
+
+	if (i == vec->size) {
+		vec->array[vec->size++] = item;
+	} else {
+		void *tmp = vec->array[i];
+		vec->array[i] = item;
+		while (i < vec->size) {
+			++i;
+			void *tmp2 = vec->array[i];
+			vec->array[i] = tmp;
+			tmp = tmp2;
+		}
+		++vec->size;
+	}
+}
+
+void *lookup_vec(size_t i, vec_t *vec)
+{
+	return vec->array[i];
+}
+
+void remove_vec(size_t i, vec_t *vec)
+{
+	for (; i < vec->size; ++i)
+		vec->array[i] = vec->array[i+1];
+	--vec->size;
+}
